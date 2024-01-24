f616caa049d09e03ef9acee462dd93943cac7cee
alexkampf <alexk764@gmail.com>
2024-01-24 17:43:07 -0500

    Movement,Camera, Walls
    Added movement script to allow player movement, added camera script to bind the camera to the player object, added walls

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+    private Rigidbody rb;
+    private float movementx;
+    private float movementy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementx = movementVector.x;
+        movementy = movementVector.y;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementx, 0.0f, movementy);
+        rb.AddForce(movement * speed);
+    }
+
+}
