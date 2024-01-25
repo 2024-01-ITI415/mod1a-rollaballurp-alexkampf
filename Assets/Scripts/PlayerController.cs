40c208fb9118f134c5f3a1dcc6a02cec182573ff
alexkampf <alexk764@gmail.com>
2024-01-24 19:15:25 -0500

    Count and count display(Need Help)
    Added counter to player script, and created UI element to display the pickup count. However, the counter is currently not displaying.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
+    public TextMeshProUGUI countText;
     private Rigidbody rb;
+    private int count;
     private float movementx;
     private float movementy;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
     }
 
 
@@ -24,10 +29,26 @@ public class PlayerController : MonoBehaviour
         movementy = movementVector.y;
     }
 
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+    }
     private void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementx, 0.0f, movementy);
         rb.AddForce(movement * speed);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count= count + 1;
+            SetCountText();
+        }
+    }
+
+    
 
 }
